a35ca0aa74912f5c7c1ff5c74657185d6a209ecd
Matheus Rodrigues <matheus.arcelino@trf1.jus.br>
2020-07-02 12:28:59 -0300

    controller de usuario

---
+++ src/main/java/com/matheus/samplerestapi/controllers/v1/UsuarioRestControlller.java
@@ -0,0 +1,27 @@
+package com.matheus.samplerestapi.controllers.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.matheus.samplerestapi.base.ApiVersions;
+import com.matheus.samplerestapi.models.entity.Usuario;
+import com.matheus.samplerestapi.repositories.UsuarioRepository;
+
+@RestController
+@RequestMapping(ApiVersions.V1 + "/usuarios")
+public class UsuarioRestControlller {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> criarUsuario(@RequestBody Usuario usuario) {
+        repository.save(usuario);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+}
